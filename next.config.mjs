795f1dce89037da011ab1868f1e7bb8ae7fb6eb5
Matthew Adebayo <olumatty@gmail.com>
2024-11-12 11:43:44 +0100

    recent

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
       },
+    ];
+  },
 };
 
 export default nextConfig;
